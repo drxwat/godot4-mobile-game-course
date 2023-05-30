f8ba272c8e7e29d1446ecef6b04451eb971a02cf
drxwat <drxwat@gmail.com>
2023-05-30 13:24:22 +0200

    5-2 adoptive HUD

--- elements/ui/hud/hud.gd
+++ elements/ui/hud/hud.gd
@@ -1,5 +1,35 @@
 extends CanvasLayer
 
+@onready var knives_counter := %KnivesCounter
+@onready var home_button := %HomeButton
+@onready var points_label := %PointsLabel
+@onready var stage_label := %StageLabel
+@onready var stage_counter := %StageCounter
+
+func _ready():
+	Events.location_changed.connect(update_hud_location)
+	update_hud_location(Events.LOCATIONS.START)
 
 func _on_home_button_pressed():
 	Events.location_changed.emit(Events.LOCATIONS.START)
+
+func update_hud_location(location: Events.LOCATIONS):
+	match location:
+		Events.LOCATIONS.START:
+			knives_counter.hide()
+			home_button.hide()
+			points_label.hide()
+			stage_label.hide()
+			stage_counter.hide()
+		Events.LOCATIONS.GAME:
+			knives_counter.show()
+			home_button.hide()
+			points_label.show()
+			stage_label.show()
+			stage_counter.show()
+		Events.LOCATIONS.SHOP:
+			knives_counter.hide()
+			home_button.show()
+			points_label.hide()
+			stage_label.hide()
+			stage_counter.hide()
